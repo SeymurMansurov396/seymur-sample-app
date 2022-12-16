9a3683ed70f56c44d818a5716fdadf8d9f83d3a5
SeymurMansurov396 <seymurmansurov168@gmail.com>
2022-12-16 21:50:48 +0400

    add  ApiGatewayApplication

---
+++ api-gateway/src/main/java/org/example/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ServerLogoutSuccessHandler handler) {
+        http
+                .authorizeExchange()
+                .pathMatchers("/", "/logout.html")
+                .permitAll()
+                .and()
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login() // to redirect to oauth2 login page.
+                .and()
+                .logout()
+                .logoutSuccessHandler(handler);
+
+        return http.build();
+    }
+
+    @Bean
+    public ServerLogoutSuccessHandler keycloakLogoutSuccessHandler(ReactiveClientRegistrationRepository repository) {
+
+        OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(repository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/logout.html");
+
+        return oidcLogoutSuccessHandler;
+    }
+}
